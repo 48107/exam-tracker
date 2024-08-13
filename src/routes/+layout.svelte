85724197e6ca934e2dbed8ccce2f53f76a135242
48107 <48107@student.stkevins.vic.edu.au>
2024-08-13 23:09:11 +1000

    developing design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
 </script>
+<title>Exam Study Tracker</title>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,28 @@
-<p class="text-3xl text-center">Hello!</p>
+<div class="bg-slate-800 py-5 rounded-lg">
+	<p class="text-3xl text-center">Practice Exams Tracker</p>
+</div>
+<div class="flex flex-row pt-4 px-3 justify-around">
+	<div class="basis-1/4 flex-row flex space-x-4 justify-center">
+		<div class="basis-60 bg-slate-800 px-6 py-4 rounded-xl hover:bg-slate-600 duration-200">
+			<p class="text-2xl text-center text-slate-50 hover:text-white">XX</p>
+			<p class="pt-2 text-sm text-center text-slate-400">days till exam</p>
+		</div>
+		<div class="basis-60 bg-slate-800 px-6 py-4 rounded-xl hover:bg-slate-600 duration-200">
+			<p class="text-2xl text-center text-slate-50 hover:text-white">XX</p>
+			<p class="pt-2 text-sm text-center text-slate-400">exams done so far</p>
+		</div>
+	</div>
+	<div class="basis-1/3 bg-slate-800 rounded-xl flex items-center hover:bg-slate-600 duration-300 hover:underline ease-in-out ">
+		<p class="basis-[100%] text-center text-2xl">Add an Exam</p>
+	</div>
+	<div class="basis-1/4 flex-row flex space-x-4 justify-between">
+		<div class="basis-60 bg-slate-800 px-6 py-4 rounded-xl hover:bg-slate-600 duration-200">
+			<p class="text-2xl text-center text-slate-50 hover:text-white">XX</p>
+			<p class="pt-2 text-sm text-center text-slate-400">hours practiced</p>
+		</div>
+		<div class="basis-60 bg-slate-800 px-6 py-4 rounded-xl hover:bg-slate-600 duration-200">
+			<p class="text-2xl text-center text-slate-50 hover:text-white">XX</p>
+			<p class="pt-2 text-sm text-center text-slate-400">exams done so far</p>
+		</div>
+	</div>
+</div>
