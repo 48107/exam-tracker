61cb08aca4560b0f7fdf8ce35abf211932336979
48107 <48107@student.stkevins.vic.edu.au>
2024-08-14 20:01:02 +1000

    Styling add and whatnot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 		</div>
 	</div>
 	<div class="basis-1/3 bg-slate-800 rounded-xl flex items-center hover:bg-slate-600 duration-300 hover:underline ease-in-out ">
-		<p class="basis-[100%] text-center text-2xl">Add an Exam</p>
+		<a class="basis-[100%] text-center text-2xl" href="/add">Add an Exam</a>
 	</div>
 	<div class="basis-1/4 flex-row flex space-x-4 justify-between">
 		<div class="basis-60 bg-slate-800 px-6 py-4 rounded-xl hover:bg-slate-600 duration-200">
@@ -26,3 +26,5 @@
 		</div>
 	</div>
 </div>
+
+
---
+++ src/routes/add/+page.svelte
@@ -0,0 +1,40 @@
+<div class="bg-slate-800 rounded-lg">
+	<p class="text-3xl px-6 pt-4 text-center">Add an Exam</p>
+	<form class="flex flex-row flex-center justify-center py-4 px-6">
+		<div>
+			<div class="basis-4 py-2">
+				<label for="name">Exam Name:</label><input
+					name="name"
+					type="text"
+					class="width-3rem bg-slate-700 border-slate-600 focus:outline-none focus:border-slate-400 border-2 focus:bg-slate-600 rounded-md px-2 ml-2"
+				/>
+			</div>
+			<div class="basis-4 py-2">
+				<label for="calcNoCalc">Exam Type (1/2):</label><input
+					name="calcNoCalc"
+					type="range"
+					min="1"
+					max="2"
+					class="ml-2"
+				/>
+			</div>
+			<div class="basis-4 py-2">
+				<label for="hours">Hours:</label><input
+					name="hours"
+					type="number"
+					min="1"
+					max="4"
+					class="width-3rem bg-slate-700 border-slate-600 focus:outline-none focus:border-slate-400 border-2 focus:bg-slate-600 rounded-md px-2 ml-2"
+				/>
+			</div>
+			<div class="basis-4 py-2">
+				<input
+					name="sumbit"
+					type="button"
+					class="width-3rem bg-slate-700 border-slate-600 border-2 rounded-md px-2 ml-2"
+                    value="Add Exam!"
+				/>
+			</div>
+		</div>
+	</form>
+</div>
